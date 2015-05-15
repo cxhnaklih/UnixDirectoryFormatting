df573f9983b56bafff36959d037b86cee1005233
cxhnaklih <junkmail@naklih.com>
2015-05-15 14:30:17 +0800

    Initial Commit

---
+++ UnixDirectoryFormatting/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace UnixDirectoryFormatting
+{
+    class Program
+    {
+        //PInvoke references to allow us to set the clipboard
+        [DllImport("user32.dll")]
+        internal static extern bool OpenClipboard(IntPtr hWndNewOwner);
+
+        [DllImport("user32.dll")]
+        internal static extern bool CloseClipboard();
+
+        [DllImport("user32.dll")]
+        internal static extern bool SetClipboardData(uint uFormat, IntPtr data);
+
+        const int CF_UNICODETEXT = 13;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+           // if an argument has been passed to the app
+            if(args.Length >0)
+            {
+                OpenClipboard(IntPtr.Zero);
+                
+                string dosPath = args[0]; // get the dos path
+                string unixPath = dosPath.Replace('\\','/'); // replace the dos slash with the unix one.
+
+                var ptr = Marshal.StringToHGlobalUni(unixPath);// put it in a pointer for use in the pinvoke call
+
+                SetClipboardData(CF_UNICODETEXT, ptr);// set the clipboard text
+
+                CloseClipboard();
+
+                Marshal.FreeHGlobal(ptr);//free the pointer.
+            }
+        }
+    }
+}
---
+++ UnixDirectoryFormatting/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UnixDirectoryFormatting")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription(@"An application to convert MS-DOS format paths to unix format paths, pastes result to clipboard")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnixDirectoryFormatting")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("46073e4d-d76b-429a-b10e-d62e571b9299")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
